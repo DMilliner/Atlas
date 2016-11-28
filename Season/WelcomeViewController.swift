7fd63021d47e24dc0f02339856f073567e9b2c3f
David Milliner <david.milliner@inwebo.com>
2016-11-28 10:32:12 +0100

    internet handling

--- Season/WelcomeViewController.swift
+++ Season/WelcomeViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreLocation
+import SystemConfiguration
+
 
 class WelcomeViewController: UIViewController, CLLocationManagerDelegate {
     var window: UIWindow?
@@ -30,7 +32,31 @@ class WelcomeViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
             locationManager.startUpdatingLocation()
         }
-
+    }
+    
+    func isInternetAvailable() -> Bool {
+        
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return false
+        }
+        
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return false
+        }
+        
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        
+        return (isReachable && !needsConnection)
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -45,68 +71,72 @@ class WelcomeViewController: UIViewController, CLLocationManagerDelegate {
         
         geoCoder.reverseGeocodeLocation(location, completionHandler: { (placemarks, error) -> Void in
             var placeMark: CLPlacemark!
+            var region: String = ""
+            var country: String = ""
+            var worldRegion: String = "World"
+
             placeMark = placemarks?[0]
             
-            print("dictionary = \(placeMark.addressDictionary)")
-            //            let region = (placeMark.timeZone?.identifier)! as String
-            let region = placeMark.timeZone?.description
-            let country = placeMark.country?.description
-            
-            print("region : \(region)")
-            print("country : \(country)")
+            if(self.isInternetAvailable()){
 
-            
-            var worldRegion: String = "World"
-            
-            if let region = region {
+                print("dictionary = \(placeMark.addressDictionary)")
+                //let region = (placeMark.timeZone?.identifier)! as String
+                
+                region = (placeMark.timeZone?.description)!
+                country = (placeMark.country?.description)!
+                
+                print("region : \(region)")
+                print("country : \(country)")
+                
+
                 if((region.range(of: "Europe")) != nil){
                     print("World region = Europe")
                     worldRegion = "Europe"
-                    
+                        
                 } else if((region.range(of: "Africa")) != nil){
                     print("World region = Africa")
                     worldRegion = "Africa"
-                    
+                        
                 } else if((region.range(of: "Asia")) != nil){
                     print("World region = Asia")
                     worldRegion = "Asia"
-                    
+                        
                 } else if((region.range(of: "Australia")) != nil){
                     print("World region = Oceania")
                     worldRegion = "Oceania"
-                    
+                        
                 } else if((region.range(of: "America")) != nil){
                     let northAmerica = ["Canada", "Mexico", "United States"]
                     let centralAmerica = ["Belize", "Costa Rica", "El Salvador", "Guatemala", "Honduras", "Nicaragua", "Panama", "Bahamas", "Barbados", "Cuba", "Dominican Republic", "Grenada", "Haiti", "Jamaica", "Trinidad Tobago"]
                     let southAmerica = ["Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "French Guinea", "Guyana", "Paraguay", "Peru", "Surinam", "Venezuela", "Uruguay"]
-                    
-                    if(northAmerica.contains(country!)){
+                        
+                    if(northAmerica.contains(country)){
                         print("World region = North America")
                         worldRegion = "North America"
-                    } else if(centralAmerica.contains(country!)){
+                    } else if(centralAmerica.contains(country)){
                         print("World region = Central America")
                         worldRegion = "Central America"
-                    } else if(southAmerica.contains(country!)){
+                    } else if(southAmerica.contains(country)){
                         print("World region = South America")
                         worldRegion = "South America"
                     }
                 } else if((region.range(of: "Pacific")) != nil){
                     print("World region = Pacific == Oceania")
                     worldRegion = "Oceania"
-                    
                 } else {
                     print("World region = somewhere else...")
                 }
+            } else {
+                print("No Internet")
             }
             
-            self.welcomeLabel.text = "Welcome to Season" + " in " + worldRegion + " ("+country!+")."
+            self.welcomeLabel.text = "Welcome to Season" + " in " + worldRegion + " ("+country+")."
             print("Country : \(country)")
-            
             UserDefaults.standard.set(worldRegion, forKey: "savedWorldRegion")
             
-            //            for element in Market().getPomeFruitsCompleteListbyLocation(location: worldRegion) {
-            //                print("Sooooooo \(element.name)")
-            //            }
+//            for element in Market().getPomeFruitsCompleteListbyLocation(location: worldRegion) {
+//                print("Sooooooo \(element.name)")
+//            }
         })
     }
     
@@ -118,5 +148,4 @@ class WelcomeViewController: UIViewController, CLLocationManagerDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 }
