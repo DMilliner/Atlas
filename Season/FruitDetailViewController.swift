2fb42c1945a8d5600798f13fade4b62ff5398c41
David Milliner <david.milliner@inwebo.com>
2016-12-14 12:35:08 +0100

    Ultimate Detail Layout

--- Season/FruitDetailViewController.swift
+++ Season/FruitDetailViewController.swift
@@ -17,9 +17,9 @@ class FruitDetailViewController: UIViewController {
     @IBOutlet weak var L_FirstViewBottom: NSLayoutConstraint!
     @IBOutlet weak var L_FirstViewTop: NSLayoutConstraint!
     @IBOutlet weak var L_FirstViewLeading: NSLayoutConstraint!
-    @IBOutlet weak var L_FirstViewTrailing: NSLayoutConstraint!
+    @IBOutlet weak var L_FirstViewTrailing_alt: NSLayoutConstraint!
     
-    @IBOutlet weak var P_FirstViewBottom: NSLayoutConstraint!
+    @IBOutlet weak var P_FirstViewBottom_alt: NSLayoutConstraint!
     @IBOutlet weak var P_FirstViewTop: NSLayoutConstraint!
     @IBOutlet weak var P_FirstViewLeading: NSLayoutConstraint!
     @IBOutlet weak var P_FirstViewTrailing: NSLayoutConstraint!
@@ -31,29 +31,29 @@ class FruitDetailViewController: UIViewController {
     @IBOutlet weak var L_SecondViewTrailing: NSLayoutConstraint!
     
     @IBOutlet weak var P_SecondViewBottom: NSLayoutConstraint!
-    @IBOutlet weak var P_SecondViewTop: NSLayoutConstraint!
+    @IBOutlet weak var P_SecondViewTop_alt: NSLayoutConstraint!
     @IBOutlet weak var P_SecondViewLeading: NSLayoutConstraint!
     @IBOutlet weak var P_SecondViewTrailing: NSLayoutConstraint!
     
     // ImageView Constraints
-    @IBOutlet weak var L_ImageViewBottom: NSLayoutConstraint!
-    @IBOutlet weak var L_ImageViewTop: NSLayoutConstraint!
+    @IBOutlet weak var L_ImageViewCenterY: NSLayoutConstraint!
     @IBOutlet weak var L_ImageViewLeading: NSLayoutConstraint!
     @IBOutlet weak var L_ImageViewTrailing: NSLayoutConstraint!
     
     @IBOutlet weak var P_ImageViewBottom: NSLayoutConstraint!
     @IBOutlet weak var P_ImageViewTop: NSLayoutConstraint!
-    @IBOutlet weak var P_ImageViewLeading: NSLayoutConstraint!
-    @IBOutlet weak var P_ImageViewTrailing: NSLayoutConstraint!
+    @IBOutlet weak var P_ImageViewCenterX: NSLayoutConstraint!
+    
+    @IBOutlet weak var ImageViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var ImageViewWidth: NSLayoutConstraint!
+    @IBOutlet weak var ImageViewAspectRatio: NSLayoutConstraint!
     
     // Label Constraints
-    @IBOutlet weak var L_LabelBottom: NSLayoutConstraint!
-    @IBOutlet weak var L_LabelTop: NSLayoutConstraint!
+    @IBOutlet weak var L_LabelCenterY: NSLayoutConstraint!
     @IBOutlet weak var L_LabelLeading: NSLayoutConstraint!
     @IBOutlet weak var L_LabelTrailing: NSLayoutConstraint!
     
-    @IBOutlet weak var P_LabelBottom: NSLayoutConstraint!
-    @IBOutlet weak var P_LabelTop: NSLayoutConstraint!
+    @IBOutlet weak var P_LabelCenterY: NSLayoutConstraint!
     @IBOutlet weak var P_LabelLeading: NSLayoutConstraint!
     @IBOutlet weak var P_LabelTrailing: NSLayoutConstraint!
     
@@ -162,74 +162,74 @@ class FruitDetailViewController: UIViewController {
     
     func applyPortraitConstraint(){
         self.view.addConstraint(self.P_FirstViewTop)
-        self.view.addConstraint(self.P_FirstViewBottom)
+        self.view.addConstraint(self.P_FirstViewBottom_alt)
         self.view.addConstraint(self.P_FirstViewLeading)
         self.view.addConstraint(self.P_FirstViewTrailing)
-        self.view.addConstraint(self.P_SecondViewTop)
+        self.view.addConstraint(self.P_SecondViewTop_alt)
         self.view.addConstraint(self.P_SecondViewBottom)
         self.view.addConstraint(self.P_SecondViewTrailing)
         self.view.addConstraint(self.P_SecondViewLeading)
         self.view.addConstraint(self.P_ImageViewTop)
         self.view.addConstraint(self.P_ImageViewBottom)
-        self.view.addConstraint(self.P_ImageViewTrailing)
-        self.view.addConstraint(self.P_ImageViewLeading)
-        self.view.addConstraint(self.P_LabelTop)
-        self.view.addConstraint(self.P_LabelBottom)
+        self.view.addConstraint(self.P_ImageViewCenterX)
+        self.view.addConstraint(self.P_LabelCenterY)
         self.view.addConstraint(self.P_LabelTrailing)
         self.view.addConstraint(self.P_LabelLeading)
         
         self.view.removeConstraint(self.L_FirstViewTop)
         self.view.removeConstraint(self.L_FirstViewBottom)
         self.view.removeConstraint(self.L_FirstViewLeading)
-        self.view.removeConstraint(self.L_FirstViewTrailing)
+        self.view.removeConstraint(self.L_FirstViewTrailing_alt)
         self.view.removeConstraint(self.L_SecondViewTop)
         self.view.removeConstraint(self.L_SecondViewBottom)
         self.view.removeConstraint(self.L_SecondViewTrailing)
         self.view.removeConstraint(self.L_SecondViewLeading)
-        self.view.removeConstraint(self.L_ImageViewTop)
-        self.view.removeConstraint(self.L_ImageViewBottom)
+        self.view.removeConstraint(self.L_ImageViewCenterY)
         self.view.removeConstraint(self.L_ImageViewTrailing)
         self.view.removeConstraint(self.L_ImageViewLeading)
-        self.view.removeConstraint(self.L_LabelTop)
-        self.view.removeConstraint(self.L_LabelBottom)
+        self.view.removeConstraint(self.L_LabelCenterY)
         self.view.removeConstraint(self.L_LabelTrailing)
         self.view.removeConstraint(self.L_LabelLeading)
+        
+        self.view.addConstraint(self.ImageViewWidth)
+        self.view.addConstraint(self.ImageViewHeight)
+        self.view.addConstraint(self.ImageViewAspectRatio)
     }
     
     func applyLandScapeConstraint(){
         self.view.removeConstraint(self.P_FirstViewTop)
-        self.view.removeConstraint(self.P_FirstViewBottom)
+        self.view.removeConstraint(self.P_FirstViewBottom_alt)
         self.view.removeConstraint(self.P_FirstViewLeading)
         self.view.removeConstraint(self.P_FirstViewTrailing)
-        self.view.removeConstraint(self.P_SecondViewTop)
+        self.view.removeConstraint(self.P_SecondViewTop_alt)
         self.view.removeConstraint(self.P_SecondViewBottom)
         self.view.removeConstraint(self.P_SecondViewTrailing)
         self.view.removeConstraint(self.P_SecondViewLeading)
         self.view.removeConstraint(self.P_ImageViewTop)
         self.view.removeConstraint(self.P_ImageViewBottom)
-        self.view.removeConstraint(self.P_ImageViewTrailing)
-        self.view.removeConstraint(self.P_ImageViewLeading)
-        self.view.removeConstraint(self.P_LabelTop)
-        self.view.removeConstraint(self.P_LabelBottom)
+        self.view.removeConstraint(self.P_ImageViewCenterX)
+        self.view.removeConstraint(self.P_LabelCenterY)
         self.view.removeConstraint(self.P_LabelTrailing)
         self.view.removeConstraint(self.P_LabelLeading)
         
         self.view.addConstraint(self.L_FirstViewTop)
         self.view.addConstraint(self.L_FirstViewBottom)
         self.view.addConstraint(self.L_FirstViewLeading)
-        self.view.addConstraint(self.L_FirstViewTrailing)
+        self.view.addConstraint(self.L_FirstViewTrailing_alt)
         self.view.addConstraint(self.L_SecondViewTop)
         self.view.addConstraint(self.L_SecondViewBottom)
         self.view.addConstraint(self.L_SecondViewTrailing)
         self.view.addConstraint(self.L_SecondViewLeading)
-        self.view.addConstraint(self.L_ImageViewTop)
-        self.view.addConstraint(self.L_ImageViewBottom)
+        self.view.addConstraint(self.L_ImageViewCenterY)
         self.view.addConstraint(self.L_ImageViewTrailing)
         self.view.addConstraint(self.L_ImageViewLeading)
-        self.view.addConstraint(self.L_LabelTop)
-        self.view.addConstraint(self.L_LabelBottom)
+        self.view.addConstraint(self.L_LabelCenterY)
         self.view.addConstraint(self.L_LabelTrailing)
         self.view.addConstraint(self.L_LabelLeading)
+        
+        self.view.addConstraint(self.ImageViewWidth)
+        self.view.addConstraint(self.ImageViewHeight)
+        self.view.addConstraint(self.ImageViewAspectRatio)
     }
 
 }
